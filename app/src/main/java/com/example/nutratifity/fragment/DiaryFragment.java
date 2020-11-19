5718be0a4336c692468e187f67927d27eb3adb42
FarisKH <farisfareed.k@gmail.com>
2020-11-19 20:00:57 +0300

    DiaryFragment

--- app/src/main/java/com/example/nutratifity/fragment/DiaryFragment.java
+++ app/src/main/java/com/example/nutratifity/fragment/DiaryFragment.java
@@ -1,12 +1,11 @@
 package com.example.nutratifity.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.nutratifity.R;
 
@@ -15,6 +14,8 @@ public class DiaryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_diary, container, false);
+        View view = inflater.inflate(R.layout.fragment_diary, container, false);
+
+        return view;
     }
 }
