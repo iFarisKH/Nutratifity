183595a21cbf43c9df95af81ddf67c77dc9f706e
FarisKH <farisfareed.k@gmail.com>
2020-11-28 17:27:34 +0300

    Search meal

--- app/src/main/java/com/example/nutratifity/AddMealActivity.java
+++ app/src/main/java/com/example/nutratifity/AddMealActivity.java
@@ -1,14 +1,48 @@
 package com.example.nutratifity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class AddMealActivity extends AppCompatActivity {
+
+    private CardView cardView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
+
+        cardView = findViewById(R.id.egg);
+        editText = findViewById(R.id.search_field);
+
+        editText.addTextChangedListener(textWatcher);
     }
+
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (editText.getText().toString().equalsIgnoreCase("egg")) {
+                cardView.setVisibility(View.VISIBLE);
+            } else {
+                cardView.setVisibility(View.INVISIBLE);
+            }
+        }
+    };
 }
